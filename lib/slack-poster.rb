dae0a7fce9f8870f76900191df8a074a519fb7c4
Ricardo Otero <oterosantos@gmail.com>
2014-01-21 20:47:43 +0000

    Version 0.0.3

--- lib/slack-poster.rb
+++ lib/slack-poster.rb
@@ -1,5 +1,4 @@
 require 'slack-poster/version'
-require 'slack-poster/message_payload'
 require 'slack-poster/poster'
 
 module Slack
--- lib/slack-poster/poster.rb
+++ lib/slack-poster/poster.rb
@@ -4,7 +4,7 @@ module Slack
   class Poster
     include HTTParty
 
-    attr_accessor :config
+    attr_accessor :username, :channel
 
     # The default format of the requests. Used on HTTP header 'Content-Type'.
     format :json
@@ -23,30 +23,48 @@ module Slack
     # HTTParty.default_params to set parameters of POST request.
     # It also accepts a configuration hash.
     #
-    # ==== Examples:
+    # ==== Examples
     #
     # Without configuration hash
     #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8')
     #
     # With configuration using username and icon avatar:
-    #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8', username: 'Im a Bot',
+    #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8', username: 'Ricardo',
     #        icon_url: 'http://www.gravatar.com/avatar/92e00fd27c64c94d04140cef88039468.png')
     #
     # You can also use an emoji as avatar:
-    #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8', username: 'Im a Bot',
+    #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8', username: 'Ricardo',
     #        icon_emoji: 'ghost')
     #
-    def initialize(team, token, config = {})
-      self.class.base_uri "https://#{team}.slack.com/services/hooks/incoming-webhook"
-      self.class.default_params token: token
+    def initialize(team, token, options = {})
+      self.class.base_uri("https://#{team}.slack.com/services/hooks/incoming-webhook")
+      self.class.default_params(token: token)
 
-      @config = config
+      @username = options[:username] || 'webhookbot'
+      @channel  = options[:channel] || '#general'
+
+      @icon_emoji = format_emoji(options[:icon_emoji])
+      @icon_url = options[:icon_url]
+
+      raise ArgumentError, 'Team name is required' if team.blank?
+      raise ArgumentError, 'Token is' if token.blank?
     end
 
-    def post(channel, text)
-      message_payload = MessagePayload.new(channel, text)
-      message = { payload: message_payload }.to_json
+    # This method will post to the configured team Slack.
+    def send_message(text)
+      payload = { text: text, channel: @channel, username: @username }.merge(avatar_hash)
       puts message
+    end
+
+    private
+
+    def avatar_hash
+      @icon_emoji ? { icon_emoji: @icon_emoji } : { icon_url: @icon_url }
+    end
+
+    def format_emoji(emoji)
+      emoji = emoji.to_s.gsub(':', '')
+      emoji.empty? ? nil : ":#{emoji}:"
     end
   end
 end
