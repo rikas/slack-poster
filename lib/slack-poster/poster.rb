d4b994562230d03dbdeb9707fd6ee896d59bfd95
Ricardo Otero <oterosantos@gmail.com>
2014-01-21 18:08:16 +0000

    Initialize comments

--- lib/slack-poster/poster.rb
+++ lib/slack-poster/poster.rb
@@ -1,6 +1,10 @@
+require 'httparty'
+
 module Slack
   class Poster
     include HTTParty
+
+    attr_accessor :config
 
     # The default format of the requests. Used on HTTP header 'Content-Type'.
     format :json
@@ -15,14 +19,34 @@ module Slack
     #
     disable_rails_query_string_format
 
-    def initialize(subdomain, token)
-      self.base_uri = "https://#{@subdomain}.slack.com/services/hooks/incoming-webhook"
-      self.default_params token: @token
+    # Initializes a Poster instance to post messages. It uses the HTTParty.base_uri and
+    # HTTParty.default_params to set parameters of POST request.
+    # It also accepts a configuration hash.
+    #
+    # ==== Examples:
+    #
+    # Without configuration hash
+    #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8')
+    #
+    # With configuration using username and icon avatar:
+    #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8', username: 'Im a Bot',
+    #        icon_url: 'http://www.gravatar.com/avatar/92e00fd27c64c94d04140cef88039468.png')
+    #
+    # You can also use an emoji as avatar:
+    #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8', username: 'Im a Bot',
+    #        icon_emoji: 'ghost')
+    #
+    def initialize(team, token, config = {})
+      self.class.base_uri "https://#{team}.slack.com/services/hooks/incoming-webhook"
+      self.class.default_params token: token
+
+      @config = config
     end
 
     def post(channel, text)
       message_payload = MessagePayload.new(channel, text)
-      puts message_payload.inspect
+      message = { payload: message_payload }.to_json
+      puts message
     end
   end
 end
