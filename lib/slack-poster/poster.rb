6438583803e2776bafc6f003f7713ddd82da14f6
Ricardo Otero <oterosantos@gmail.com>
2015-05-08 22:44:29 +0200

    Updated comments

--- lib/slack-poster/poster.rb
+++ lib/slack-poster/poster.rb
@@ -16,26 +16,26 @@ module Slack
     #
     # Would translate to this:
     #   => /?selected_ids[]=1&selected_ids[]=2&selected_ids[]=3
-    #
     disable_rails_query_string_format
 
-    # Initializes a Poster instance to post messages. It uses the HTTParty.base_uri and
-    # HTTParty.default_params to set parameters of POST request.
-    # It also accepts a configuration hash.
+    # Initializes a Poster instance to post messages with an incoming webhook URL.
+    # It also accepts an options hash. If no options are given then the poster uses the default
+    # configuration from Slack integration.
     #
     # ==== Examples
     #
-    # Without configuration hash
-    #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8')
+    # Without options
+    #   => Slack::Poster.new('https://hooks.slack.com/services/T044G6VBA//TCIzZQQd7IKhQzCKc6W310va')
     #
-    # With configuration using username and icon avatar:
-    #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8', username: 'Ricardo',
+    # With options using a custom username and icon avatar:
+    #   => Slack::Poster.new('https://hooks.slack.com/services/T044G6VBA//TCIzZQQd7IKhQzCKc6W310va',
+    #        username: 'Ricardo',
     #        icon_url: 'http://www.gravatar.com/avatar/92e00fd27c64c94d04140cef88039468.png')
     #
     # You can also use an emoji as avatar:
-    #   => Slack::Poster.new('myteam', 'eNmZHQY6f591ziHyZdzePFz8', username: 'Ricardo',
+    #   => Slack::Poster.new('https://hooks.slack.com/services/T044G6VBA//TCIzZQQd7IKhQzCKc6W310va',
+    #        username: 'Ricardo',
     #        icon_emoji: 'ghost')
-    #
     def initialize(webhook_url, options = {})
       self.class.base_uri(webhook_url)
 
@@ -46,14 +46,24 @@ module Slack
       raise ArgumentError, 'Webhook URL is required' if webhook_url.nil?
     end
 
+    # Sends a message to Slack. The message can be either plain text or a Slack::Message object.
+    #
+    # ==== Examples
+    #
+    # Plain text message
+    #   => poster.send_message('hello world')
+    #
+    # Using a message object
+    #   => poster.send_message(Slack::Message.new(text: 'hello world'))
+    #
+    # You can have messages with attachments if you build your message with a Slack::Message object
+    # and add Slack::Attachment objects.
     def send_message(message)
       body = message.is_a?(String) ? options.merge(text: message) : options.merge(message.as_json)
 
       attach_extras(body) unless attachments.empty?
 
       response = self.class.post('', { body: { payload: body.to_json }})
-
-      puts body.to_json
 
       "#{response.body} (#{response.code})"
     end
