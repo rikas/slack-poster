188925d068e013f809350851ecedc177e4a8ba76
Ricardo Otero <oterosantos@gmail.com>
2015-05-24 20:12:47 +0100

    First version of spec for Slack::Attachment

--- spec/attachment_spec.rb
+++ spec/attachment_spec.rb
@@ -1,4 +1,34 @@
-RSpec.describe Slack::Attachment do
+require 'spec_helper'
+
+describe Slack::Attachment do
+  describe '.new' do
+    let(:options) do
+      {
+        fallback: 'fallback',
+        text: 'text',
+        title: 'title',
+        title_link: 'title_link',
+        image_url: 'image_url',
+        color: 'color',
+        pretext: 'pretext'
+      }
+    end
+
+    it 'accepts no attributes' do
+      att = described_class.new
+
+      expect(att).to_not be_nil
+    end
+
+    it 'initializes all possible option' do
+      att = described_class.new(options)
+
+      options.each do |key, value|
+        expect(att.send(key)).to eq(value)
+      end
+    end
+  end
+
   describe '#add_field' do
     context 'when short is set to true' do
     end
