d2f581f48d1fea7b833a0149215d5948e0589bed
Ricardo Otero <oterosantos@gmail.com>
2015-05-24 19:41:23 +0100

    First tests for Slack::Message

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,9 +1,42 @@
 require 'spec_helper'
 
-RSpec.describe Slack::Message do
+describe Slack::Message do
+  let(:message) { described_class.new('hello') }
+
   describe '#add_attachment' do
+    it 'adds an attachment correctly' do
+      attachment = Slack::Attachment.new
+
+      message.add_attachment(attachment)
+
+      expect(message.attachments).to eq([attachment])
+    end
+
+    it 'adds multiple attachments correctly' do
+      att1 = Slack::Attachment.new
+      att2 = Slack::Attachment.new
+
+      message.add_attachment(att1)
+      message.add_attachment(att2)
+
+      expect(message.attachments).to eq([att1, att2])
+    end
   end
 
   describe '#as_json' do
+    context 'when no attachments were added' do
+      it 'returns the correct json' do
+        expect(message.as_json).to eq(text: "hello")
+      end
+    end
+
+    context 'when attachments are added' do
+      it 'returns the correct json' do
+        att1 = Slack::Attachment.new
+        att2 = Slack::Attachment.new
+
+        message.attachments = [att1, att2]
+      end
+    end
   end
 end
